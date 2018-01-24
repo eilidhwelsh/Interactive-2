2161475b5ea594e9935fd5bbbbcdc57fcd910b27
eilidhwelsh <31681658+eilidhwelsh@users.noreply.github.com>
2018-01-23 16:34:48 -0800

    hover4

--- exercise/exercise-charrette/assets/script.js
+++ exercise/exercise-charrette/assets/script.js
@@ -1,10 +1,9 @@
-* $(document).ready(function(){
+ $(document).ready(function(){
 
 	$('#button-3').click(function() {
 		$(this).animate({
 			opacity: 0.4,
-			marginLeft: '0.6in',
-			fontSize: '24px',
+			fontSize: '24px'
 			borderWidth: '10px'
 		}, 500 );
 	});
